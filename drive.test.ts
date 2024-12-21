2c1021f8eea467237c575977a2cb60a6215b5665
hugoalh <hugoalh@gmail.com>
2024-12-21 18:51:19 +0800

    Update test

--- drive.test.ts
+++ drive.test.ts
@@ -1,8 +1,16 @@
 import { getDriveInfo } from "./drive.ts";
-Deno.test("Main", {
+Deno.test("PowerShell Core", {
 	permissions: {
 		run: ["pwsh"]
 	}
 }, async () => {
 	console.log(await getDriveInfo());
+});
+Deno.test("PowerShell Desktop", {
+	ignore: Deno.build.os !== "windows",
+	permissions: {
+		run: ["powershell"]
+	}
+}, async () => {
+	console.log(await getDriveInfo({ powershellPath: "powershell" }));
 });
