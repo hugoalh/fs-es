e8a0b1694e9060e766a3a0db73247f44711b55c0
hugoalh <hugoalh@gmail.com>
2025-01-15 17:07:55 +0800

    Update test

--- stream.test.ts
+++ stream.test.ts
@@ -1,28 +1,59 @@
 import { readFileAsChunksSync } from "./stream.ts";
-Deno.test("ReadFileAsChunks Standard", {
+Deno.test("ReadFileAsChunks 64 Standard", {
 	permissions: {
 		read: true
 	}
 }, () => {
-	let chunksCount = 0n;
+	let bytes = 0n;
+	let chunks = 0n;
 	for (const chunk of readFileAsChunksSync("./README.md", { chunkSize: 64 })) {
-		chunksCount += 1n;
-		console.log(chunk);
+		bytes += BigInt(chunk.length);
+		chunks += 1n;
 	}
-	console.debug(`Chunks: ${chunksCount}`);
+	console.debug(`Bytes: ${bytes}; Chunks: ${chunks}`);
 });
-Deno.test("ReadFileAsChunks Reduce", {
+Deno.test("ReadFileAsChunks 64 Reduce", {
 	permissions: {
 		read: true
 	}
 }, () => {
-	let chunksCount = 0n;
+	let bytes = 0n;
+	let chunks = 0n;
 	for (const chunk of readFileAsChunksSync("./README.md", {
 		chunkSize: 64,
 		reduceChunks: true
 	})) {
-		chunksCount += 1n;
-		console.log(chunk);
+		bytes += BigInt(chunk.length);
+		chunks += 1n;
 	}
-	console.debug(`Chunks: ${chunksCount}`);
+	console.debug(`Bytes: ${bytes}; Chunks: ${chunks}`);
+});
+Deno.test("ReadFileAsChunks 1024 Standard", {
+	permissions: {
+		read: true
+	}
+}, () => {
+	let bytes = 0n;
+	let chunks = 0n;
+	for (const chunk of readFileAsChunksSync("./README.md", { chunkSize: 1024 })) {
+		bytes += BigInt(chunk.length);
+		chunks += 1n;
+	}
+	console.debug(`Bytes: ${bytes}; Chunks: ${chunks}`);
+});
+Deno.test("ReadFileAsChunks 1024 Reduce", {
+	permissions: {
+		read: true
+	}
+}, () => {
+	let bytes = 0n;
+	let chunks = 0n;
+	for (const chunk of readFileAsChunksSync("./README.md", {
+		chunkSize: 1024,
+		reduceChunks: true
+	})) {
+		bytes += BigInt(chunk.length);
+		chunks += 1n;
+	}
+	console.debug(`Bytes: ${bytes}; Chunks: ${chunks}`);
 });
