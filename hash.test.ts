10324865e2190c92154a8041e4ce98d7f1a22b32
hugoalh <hugoalh@gmail.com>
2025-01-20 11:52:00 +0800

    Implement function `getHashSync`

--- hash.test.ts
+++ hash.test.ts
@@ -1,8 +1,22 @@
-import { getHash } from "./hash.ts";
+import { assertEquals } from "STD/assert/equals";
+import {
+	getHash,
+	getHashSync
+} from "./hash.ts";
 Deno.test("Main", {
 	permissions: {
 		read: [Deno.cwd()]
 	}
-}, async () => {
-	console.log(await getHash(Deno.cwd()));
+}, async (t) => {
+	let resultAsync: string;
+	let resultSync: string;
+	await t.step("Async", async () => {
+		resultAsync = await getHash(Deno.cwd());
+	});
+	console.log(resultAsync!);
+	await t.step("Sync", () => {
+		resultSync = getHashSync(Deno.cwd());
+	});
+	console.log(resultSync!);
+	assertEquals(resultSync!, resultAsync!);
 });
